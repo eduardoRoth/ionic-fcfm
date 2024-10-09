45eed24215af13b5092c62fc31134c20292af0e1
Eduardo Roth <eduardo@eduardoroth.dev>
2024-10-09 13:20:55 -0600

    chore(): ios and android files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.eduardoroth.fcfm',
+  appName: 'ionic-fcfm',
+  webDir: 'dist/ionic-fcfm/browser'
+};
+
+export default config;
